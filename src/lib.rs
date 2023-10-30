f2e192a17c5163b31da35d16e6fe800cbbbcbed9
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-10-30 14:57:38 +0100

    Update to 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,10 @@
-use chrono::{DateTime, NaiveDateTime, Utc};
-use std::time::SystemTime;
+pub mod datetime_utils;
+pub mod kws_decrypt;
 
-pub struct DateTimeUtils {}
+pub use datetime_utils::DateTimeUtils;
+pub use datetime_utils::ToDateTimeString;
+pub use kws_decrypt::decrypt_data_with_kms;
 
 pub trait HasId {
     fn id(&self) -> Option<u32>;
-}
-
-pub trait ToDateTimeString {
-    fn to_datetime_string(self) -> String;
-}
-
-impl ToDateTimeString for i64 {
-    fn to_datetime_string(self) -> String {
-        let naive_datetime = NaiveDateTime::from_timestamp_opt(self, 0).expect("Invalid timestamp");
-        naive_datetime.format("%Y-%m-%d %H:%M:%S").to_string()
-    }
-}
-
-impl ToDateTimeString for SystemTime {
-    fn to_datetime_string(self) -> String {
-        let datetime: DateTime<Utc> = self.into();
-        datetime.format("%Y-%m-%d %H:%M:%S").to_string()
-    }
-}
-
-impl DateTimeUtils {
-    pub fn get_current_datetime_string() -> String {
-        let current_time = Utc::now().timestamp();
-        let naive_datetime =
-            NaiveDateTime::from_timestamp_opt(current_time, 0).expect("Invalid timestamp");
-        naive_datetime.format("%Y-%m-%d %H:%M:%S").to_string()
-    }
-
-    pub fn get_current_date_string() -> String {
-        let current_time = Utc::now().timestamp();
-        let naive_datetime =
-            NaiveDateTime::from_timestamp_opt(current_time, 0).expect("Invalid timestamp");
-        naive_datetime.format("%Y-%m-%d").to_string()
-    }
 }
