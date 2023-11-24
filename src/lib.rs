acd37dfeba8f898df26fca17012781aa5a2e3aba
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-11-24 19:44:11 +0100

    Add math lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod datetime_utils;
 pub mod kws_decrypt;
+pub mod math;
 
 pub use datetime_utils::DateTimeUtils;
 pub use datetime_utils::ToDateTimeString;
 pub use kws_decrypt::decrypt_data_with_kms;
+pub use math::*;
 
 pub trait HasId {
     fn id(&self) -> Option<u32>;
---
+++ src/math.rs
@@ -0,0 +1,3 @@
+pub fn exponential_decay(x: f64, a: f64) -> f64 {
+    f64::exp(-a * x)
+}
