5d98025bc4ca20469f2855a635dc95c441dbf2f0
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-01-22 10:48:16 +0100

    Add parse_to_f64()

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,11 @@ pub use datetime_utils::DateTimeUtils;
 pub use datetime_utils::ToDateTimeString;
 pub use kws_decrypt::decrypt_data_with_kms;
 pub use math::*;
-
+use std::num::ParseFloatError;
 pub trait HasId {
     fn id(&self) -> Option<u32>;
+}
+
+pub fn parse_to_f64(input: &str) -> Result<f64, ParseFloatError> {
+    input.parse::<f64>()
 }
