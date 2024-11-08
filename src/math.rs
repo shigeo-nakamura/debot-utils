1bb1a79313131932cfd3e3475beaca1543a53868
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-11-08 22:04:01 +0100

    Add random_bool()

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,10 @@
+use rand::Rng;
+
 pub fn exponential_decay(x: f64, a: f64) -> f64 {
     f64::exp(-a * x)
+}
+
+pub fn random_bool(probability: f64) -> bool {
+    let mut rng = rand::thread_rng();
+    rng.gen_bool(probability)
 }
