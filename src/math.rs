5bb2f68ca6905e7a317f21cff97a511728942fad
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2024-11-11 13:38:50 +0100

    Add possibility to specify a seed

--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,13 @@
-use rand::Rng;
+use rand::{rngs::StdRng, Rng, RngCore, SeedableRng};
 
 pub fn exponential_decay(x: f64, a: f64) -> f64 {
     f64::exp(-a * x)
 }
 
-pub fn random_bool(probability: f64) -> bool {
-    let mut rng = rand::thread_rng();
+pub fn random_bool(probability: f64, seed: Option<u64>) -> bool {
+    let mut rng: Box<dyn RngCore> = match seed {
+        Some(seed_value) => Box::new(StdRng::seed_from_u64(seed_value)),
+        None => Box::new(rand::thread_rng()),
+    };
     rng.gen_bool(probability)
 }
